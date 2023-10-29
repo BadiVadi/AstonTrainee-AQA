39d984b6eaee392223dd199a5ef62ed3c364b665
Вадим Ильинец <Badivadid2@gmail.com>
2023-10-29 17:41:45 +0300

    Lesson 4 (Exercise 1-2, 3)

--- Lesson2/src/main/java/org/example/Exercice2.java
+++ Lesson2/src/main/java/org/example/Exercice2.java
@@ -8,10 +8,9 @@ public class Exercice2 {
     private static void checkSumSign() {
         int a = 0;
         int b = 1;
-        if(a + b >= 0){
+        if (a + b >= 0) {
             System.out.println("Сумма положительная");
-        }
-        else{
+        } else {
             System.out.println("Сумма отрицательная");
         }
     }
