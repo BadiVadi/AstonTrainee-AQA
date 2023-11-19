a850d3ff47e6a9c4b575db81cebe401a29badc0d
Вадим Ильинец <Badivadid2@gmail.com>
2023-11-19 17:36:13 +0300

    Lesson 9

---
+++ Les9/src/main/java/org/example/Logins.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Logins {
+    public static void main(String[] args) {
+        List<String> logins = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Напишите логин и нажмите Enter для ввода: ");
+        while (true) {
+            String login = scanner.nextLine().trim();
+            if (login.isEmpty()) {
+                break;
+            }
+            logins.add(login);
+        }
+
+        System.out.println("Логины на букву 'f': ");
+        for (String login : logins) {
+            if (login.startsWith("f")) {
+                System.out.println(login);
+            }
+        }
+    }
+}
