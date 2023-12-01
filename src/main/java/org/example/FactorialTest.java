383375ab28fd48093e1af39822dd90fe518447e2
Вадим Ильинец <Badivadid2@gmail.com>
2023-12-01 17:32:33 +0300

    AQA Lesson 12_Testing

--- src/main/java/org/example/FactorialTest.java
+++ src/main/java/org/example/FactorialTest.java
@@ -1,11 +1,14 @@
 package org.example;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class FactorialTest {
 
     @Test
     void testCalculateFactorial() {
-        assertEquals(120, Factorial.calculateFactorial(5));
+int input = 5;
+    int expectedOutput = 120;
+    int resultOutput = Factorial.calculateFactorial(input);
+    Assert.assertEquals(resultOutput, expectedOutput, "Ошибочный расчет");
     }
 }
