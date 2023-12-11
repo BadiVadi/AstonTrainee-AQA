cf35944c7153535b97a90ba2e5075549e5f3d4f9
Вадим Ильинец <Badivadid2@gmail.com>
2023-12-11 21:51:34 +0300

    AQA_Maven Lesson 15

--- src/test/java/org/example/WBTest.java
+++ src/test/java/org/example/WBTest.java
@@ -1,15 +1,16 @@
 package org.example;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+
 import static org.example.DriverHelper.driver;
-import static org.example.DriverHelper.getWebDriver;
 
 public class WBTest extends BaseTest {
 //    WBPage wbPage = new WBPage();
@@ -34,8 +35,8 @@ public class WBTest extends BaseTest {
 
         actions.sendKeys(Keys.ENTER).perform();
 
-        WebElement addToBasketitem1 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class= 'btn-main']")));
-        addToBasketitem1.click();
+        WebElement addToBasketItem1 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class= 'btn-main']")));
+        addToBasketItem1.click();
 
         try {
             Thread.sleep(5000);
@@ -49,11 +50,48 @@ public class WBTest extends BaseTest {
 
         actions.sendKeys(Keys.ENTER).perform();
 
-        WebElement addToBasketitem2 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class= 'btn-main']")));
-        addToBasketitem2.click();
+        WebElement addToBasketItem2 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class= 'btn-main']")));
+        addToBasketItem2.click();
 
 
-        WebElement goToBasket = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@class ='navbar-pc__icon navbar-pc__icon--basket']")));
+        WebElement goToBasket = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class ='order']//a[@class]")));
         goToBasket.click();
+
+        // ПОИСК ТОВАРА В КОРЗИНЕ
+        WebElement findItems = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='good-info__good-name']")));
+
+        List<WebElement> itemsInBasket = findItems.findElements(By.xpath("//span[@class='good-info__good-name']"));
+        StringBuilder itemsInBasketText = new StringBuilder();
+
+        for (WebElement item : itemsInBasket) {
+            itemsInBasketText.append(item.getText()).append("\n");
+        }
+        System.out.println(itemsInBasketText);
+
+        // ПОИСК КОЛИЧЕСТВА ТОВАРА В КОРЗИНЕ
+        WebElement itemCounter = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class = 'count__wrap']//input")));
+
+        List<WebElement> numberOfItemsInBasket = itemCounter.findElements(By.xpath("//div[@class = 'count__wrap']//input"));
+        StringBuilder numberOfItemsInBasketText = new StringBuilder();
+
+        for (WebElement quantity : numberOfItemsInBasket) {
+            numberOfItemsInBasketText.append(quantity.getAttribute("value")).append("\n");
+        }
+        System.out.println(numberOfItemsInBasketText);
+
+        //ПОИСК ЦЕНЫ ТОВАРА В КОРЗИНЕ
+        WebElement itemCost = driver.findElement(By.xpath("//div[@class = 'list-item__price-new']"));
+
+        List<WebElement> costOfEachItem = itemCost.findElements(By.xpath("//div[@class = 'list-item__price-new']"));
+        StringBuilder costOfEachItemText = new StringBuilder();
+
+        for (WebElement cost : costOfEachItem) {
+            costOfEachItemText.append(cost.getAttribute("textContent")).append("\n");
+        }
+        System.out.println(costOfEachItemText);
+
+        //ОБЩАЯ СТОИМОСТЬ КОРЗИНЫ
+        WebElement generalItemsCost = driver.findElement(By.xpath("//div[@class='sidebar__sticky-wrap']//span[@class='b-right']"));
+        System.out.println("Общая стоимость товаров: " + generalItemsCost.getAttribute("textContent"));
     }
 }
